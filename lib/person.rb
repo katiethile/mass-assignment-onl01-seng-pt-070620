b1da6c819c3ce666d423d71aa599a5b8428eda96
Katie Le <lethikatie@gmail.com>
2020-08-07 20:58:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,7 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-def 
+def initialize(attributes)
+  attributes.each {|key, value| self.send(())
 
 end
